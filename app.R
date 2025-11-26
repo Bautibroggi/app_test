6090ae3157117dc321480245e38aad09c6fbc4d0
bautista broggini <bautistabroggini@gmail.com>
2025-11-26 09:34:57 -0300

    La traducción

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Old es español"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
